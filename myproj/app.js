8d69bc1998ff82c5421f5606616465794c13c50f
Alec Thompson <alect3@illinois.edu>
2024-05-02 10:39:51 -0500

    topclasses set up. error on the SQL query portion.

--- myproj/app.js
+++ myproj/app.js
@@ -11,19 +11,10 @@ var bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var topClassesRouter = require('./routes/topclasses');
 var connection  = require('./database');
 
 var app = express();
-
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'ejs');
-
-// app.use(logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -42,73 +33,24 @@ app.use(session({
   cookie: { maxAge: 60000 }
 }))
 
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
-
-// // Database connection setup
-// var db = mysql.createConnection({
-//   host: '34.173.61.66',  // GCP database host
-//   user: 'root',  //  database username
-//   password: 'acY5q*r0B+#g_u-U',  // database password
-//   database: 'uiuc_course_hub', // database name
-//   connectTimeout: 10000 
-// });
-
 app.use(flash());
 
 app.use('/', indexRouter);
 app.use('/list', usersRouter);
+app.get('/helloworld', (req, res) => {
+  // Send the HTML file as the response
+  res.sendFile(path.join(__dirname, "public",'helloworld.html'));
+});
+app.get('/userfeedback', (req, res) => {
+  // Send the HTML file as the response
+  res.sendFile(path.join(__dirname, "public",'userfeedback.html'));
+});
 
-// Connect to the database
-// db.connect(function(err) {
-//   if (err) throw err;
-//   console.log("Connected to database!");
-// });
-// db.connect(function(err) {
-//   if (err) {
-//     console.error('Error connecting to the database: ' + err.stack);
-//     return;
-//   }
-//   console.log("Connected to database!");
-// });
-
-
-// // Route to handle the search form submission and fetch data based on user-entered CRN
-// app.get('/search', function(req, res, next) {
-//   // Extract the CRN from the query string
-//   const userCRN = req.query.CRN;
-//   const courseTitle = req.query.courseTitle || '';
-//   const professorName = req.query.professorName || '';
-
-//   // Validate that the CRN is a numeric value
-//   if (!userCRN || isNaN(userCRN)) {
-//     return res.status(400).json({ message: 'A valid CRN is required' });
-//   }
-
-//   // Construct the query
-//  const query = 'SELECT CourseName, CRN, CourseNum, Avg_Grade, Instructor, Semester, Year FROM GPAHistory WHERE CRN = ? and CourseName LIKE ? and Instructor LIKE ?';
-  
-//   const queryParams = [
-//         userCRN,
-//         `%${courseTitle}%`,
-//         `%${professorName}%`
-//     ];
-
-//   // Execute the query with the CRN parameter
-//   db.query(query, queryParams, function(err, results) {
-//     if (err) {
-//       // Handle any database errors
-//       return next(err);
-//     }
-//     // Send the results back to the client
-//     if (results.length > 0) {
-//       res.json(results);
-//     } else {
-//       // If no results found, send an appropriate response
-//       res.status(404).json({ message: 'No records found for the provided CRN' });
-//     }
-//   });
-// });
+app.use('/search_topclasses', topClassesRouter);
+app.get('/topclasses', (req, res) => {
+  // Send the HTML file as the response
+  res.sendFile(path.join(__dirname, 'public','topclasses.html'));
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -127,10 +69,6 @@ app.use(function(err, req, res, next) {
 });
 
 
-// const PORT = 8080; 
-// app.listen(8080, '0.0.0.0', () => {
-//     console.log(`Server running on port 8080`);
-// });
 app.listen(4000, function () {
   console.log('Node app is running on port 4000');
 });
---
+++ myproj/routes/topclasses.js
@@ -0,0 +1,76 @@
+// var express = require('express');
+// var router = express.Router();
+
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+// module.exports = router;
+
+var express = require('express');
+var router = express.Router();
+var connection  = require('../database.js');
+
+
+/* GET home page. */
+// router.get('/', function(req, res, next) {
+	 
+//  connection.query('SELECT * FROM users ORDER BY id desc',function(err,rows)     {
+
+//         if(err){
+//          req.flash('error', err); 
+//          res.render('list',{page_title:"Users - Node.js",data:''});   
+//         }else{
+           
+//             res.render('list',{page_title:"Users - Node.js",data:rows});
+//         }
+                           
+//          });
+       
+//     });
+
+
+
+
+// Route to handle the search form submission and fetch data based on user-entered CRN
+router.get('/', function(req, res, next) {
+  // Extract the CRN from the query string
+  const dept = req.query.DepartmentCode;
+  const min_gpa = req.query.GPAFloor;
+  const min_rating = req.query.CourseRatingFloor;
+  const credit_hours = req.query.CreditHours;
+  const intersect_union = req.query.IntersectOrUnion;
+
+
+  // Validate that the CRN is a numeric value
+  // if (!userCRN || isNaN(userCRN)) {
+  //   return res.status(400).json({ message: 'A valid CRN is required' });
+  // }
+
+  // Construct the query
+ const query = 'SELECT * FROM UserCustomQuery(?,?,?,?,?)';
+ //Alawini, Abdussalam A
+ //CRN = ? and CourseName LIKE ? and 
+  const queryParams = [
+        dept, min_gpa, min_rating, credit_hours, intersect_union
+    ];
+
+  // Execute the query with the CRN parameter
+  connection.query(query, queryParams, function(err, results) {
+    if (err) {
+      // Handle any database errors
+      return next(err);
+    }
+    // Send the results back to the client
+    if (results.length > 0) {
+      res.render('list_topclasses',{page_title:"Top Classes",data:results});
+      //res.json(results);
+    } else {
+      // If no results found, send an appropriate response
+      res.status(404).json({ message: 'No records found for the provided CRN' });
+    }
+  });
+});
+
+module.exports = router;
