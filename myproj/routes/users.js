e42770ff9810c13d880269a9f2553722353ed473
Alec Thompson <alect3@illinois.edu>
2024-05-02 22:55:29 -0500

    delete of last submitted thing works

--- myproj/routes/users.js
+++ myproj/routes/users.js
@@ -10,16 +10,16 @@ var connection  = require('../database.js');
 router.use(express.json());
 
 //Post Endpoint for submitting course review
-router.post('/', function(req, res) {
+router.post('/submitReview', function(req, res) {
   const { email, crn, year, semester, testimony, rating, difficulty, timeCommit } = req.body;
 
   // SQL Query to insert data
-  const sql = `
+  const submit_sql = `
     INSERT INTO UserFeedback (Email, Year, CRN, Semester, Testimony, Rating, Difficulty, TimeCommit, Time_stamp)
     VALUES (?, ?, ?, ?, ?, ?, ?, ?, CURRENT_TIMESTAMP)
   `;
 
-    connection.query(sql, [email, year, crn, semester, testimony, rating, difficulty, timeCommit], function(err, results, fields){
+    connection.query(submit_sql, [email, year, crn, semester, testimony, rating, difficulty, timeCommit], function(err, results, fields){
       if (err) {
         // Handle any database errors
         console.error('Failed to insert data:', err);
@@ -31,6 +31,59 @@ router.post('/', function(req, res) {
       }
       res.json({
           message: "Review submitted successfully",
+          data: req.body
+      });
+    });
+});
+
+router.delete('/deleteReview', function(req, res) {
+  const { email, crn, year, semester} = req.body;
+
+  // SQL Query to insert data
+  const delete_sql = `
+    DELETE FROM UserFeedback WHERE Email = ? AND CRN = ? AND Year = ? AND Semester = ?
+  `;
+  console.log(email);
+  console.log(crn);
+  console.log(year);
+  console.log(semester);
+    connection.query(delete_sql, [email, crn, year, semester], function(err, results, fields){
+      if (err) {
+        // Handle any database errors
+        console.error('Failed to delete data:', err);
+        res.status(500).json({
+            message: "Failed to delete review",
+            error: err.message
+        });
+        return next(err);
+      }
+      res.json({
+          message: "Review deleted successfully",
+          data: req.body
+      });
+    });
+});
+
+router.post('/updateReview', function(req, res) {
+  const { email, crn, year, semester, testimony, rating, difficulty, timeCommit } = req.body;
+
+  // SQL Query to insert data
+  const sql = `
+    UPDATE UserFeedback WHERE Email = ? AND CRN = ? AND Year = ? AND Semester = ?
+  `;
+
+    connection.query(sql, [email, year, crn, semester], function(err, results, fields){
+      if (err) {
+        // Handle any database errors
+        console.error('Failed to delete data:', err);
+        res.status(500).json({
+            message: "Failed to delete review",
+            error: err.message
+        });
+        return next(err);
+      }
+      res.json({
+          message: "Review deleted successfully",
           data: req.body
       });
     });
